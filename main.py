2197a2ca098675c52091f2e627a7aec9506408c1
Allyson Maciel Guimarães <bsinet@hotmail.com>
2022-12-13 01:30:23 -0300

    wip

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import whisper
-
 import models
+import os
+
 from worker import Worker
 from models import Task
-
-from params.create_task import CreateTask
-
 from fastapi import FastAPI
+
+from playhouse.shortcuts import model_to_dict
+from params.create_task import CreateTask
+from starlette.responses import FileResponse
+
 
 with models.base_model.db:
     models.base_model.db.create_tables([Task])
@@ -17,20 +20,22 @@ app = FastAPI()
 worker.start()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/tasks/{item_id}")
-def read_item(item_id: int, q: None):
-    return {"item_id": item_id, "q": q}
+@app.get("/tasks/{id}")
+def read_item(id: int,):
+    task = Task.get(Task.id == id)
+    return model_to_dict(task)
 
 
 @app.post("/tasks")
 def create(task: CreateTask):
-    Task.create(source_url=task.source_url)
-    return task
+    task = Task.create(source_url=task.source_url)
+    return model_to_dict(task)
 
 
+@app.get("/captions/{id}")
+def get_caption(id: str):
+    task = Task.get(Task.id == id)
 
+    file_path = f"{ os.path.join('storage', task.ref_id) }.srt"
+
+    return FileResponse(file_path, media_type='application/octet-stream', filename=f"{task.ref_id}.srt")
--- worker.py
+++ worker.py
@@ -1,3 +1,4 @@
+import logging
 from models import Task
 from time import sleep
 from threading import Thread
@@ -8,9 +9,11 @@ class Worker(Thread):
     def run(self):
         while True:
             try:
+                logging.info("Processing...")
                 self.process()
-            except:
-                pass
+                logging.info("Done.")
+            except Exception as e:
+                logging.warning(str(e))
 
             sleep(10)
 
@@ -19,11 +22,16 @@ class Worker(Thread):
 
     def process(self):
         task = self.get_task_available()
+
+        if task is None:
+            return
+
         task.set_as_processing()
         task_processor = TaskProcessor(task)
 
         try:
             caption_ref_id = task_processor.execute()
             task.set_as_finished(caption_ref_id)
-        except:
+        except Exception as e:
+            logging.warning(str(e))
             task.set_as_failed()
